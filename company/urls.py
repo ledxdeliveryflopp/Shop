7c93c164d52d98fc5943ac9fc9c1154d960b2704
ledxdeliveryflopp <108229226+ledxdeliveryflopp@users.noreply.github.com>
2022-12-13 12:52:59 +0700

    Delete company directory

--- company/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import AllCompanyView
-
-app_name = 'company'
-
-urlpatterns = [
-    path('<int:pk>/', AllCompanyView.as_view(), name='all_company'),
-]
--- company/views.py
+++
@@ -1,9 +0,0 @@
-from django.views.generic import ListView
-
-from .models import Company
-
-
-class AllCompanyView(ListView):
-    model = Company
-    queryset = Company.objects.filter(is_active=True)
-    # template_name = 'company/all_company.html'
